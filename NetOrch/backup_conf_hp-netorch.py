e14eec69dc95910e47d2829f504c3fa2433666f3
Vinicius-python <64868064+Vinicius-python@users.noreply.github.com>
2024-05-20 22:26:47 -0300

    Update backup_conf_hp-netorch.py

--- NetOrch/backup_conf_hp-netorch.py
+++ NetOrch/backup_conf_hp-netorch.py
@@ -70,6 +70,6 @@ def backup_switch(ip, username, password, output_file):
 with open('/usr/local/scripts/routers_hp-netorch', 'r') as routers_file:
     for line in routers_file:
         name, ip = line.strip().split(':')
-        username = 'imc'
-        password = 'Ziva@imc'
+        username = 'admin'
+        password = 'admin'
         backup_switch(ip, username, password, f"/var/www/html/netorch/hp/config-backup_{name}.{current_timestamp}.cfg")
